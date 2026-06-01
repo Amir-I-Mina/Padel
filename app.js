3af41c02d3745ccf0fc035a8dc0b7ed28473575d
PatrickZakaria <205521718+patrick00051@users.noreply.github.com>
2026-06-02 00:23:49 +0300

    change the html to ejs

--- app.js
+++ app.js
@@ -3,26 +3,36 @@ const app = express();
 const mongoose = require('mongoose');
 const fileUpload = require('express-fileupload');
 const path = require('path');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const Coach = require('./models/CoachModels');
 const User = require('./models/UserModel');
 const BookingAcademy = require('./models/BookingAcademy');
 const courts = require('./models/courtsBoking');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 
-const productRoutes = require('./routes/productRoutes');
+
+const productRoutes = require('./routes/ProductsRoutes');
 app.use('/products', productRoutes);
-
 const adminRoutes = require('./routes/adminRoutes');
 app.use('/admin', adminRoutes);
-
-const dbURI = 'mongodb://Padelweb:padel12345@...';
-
-const orderRoutes = require('./routes/orderRoutes');
+const orderRoutes = require('./routes/order');
 app.use('/orders', orderRoutes);
+
+
+const academyRoutes = require("./routes/academyRoutes");
+const academyAdminRoutes = require("./routes/academyAdminRoutes");
+
+app.use("/academy", academyRoutes);
+app.use("/academy/admin", academyAdminRoutes);
+
+
+
+
+
+const dbURI = 'mongodb://Padelweb:<db_password>@ac-tgk4oui-shard-00-00.dfnd8so.mongodb.net:27017,ac-tgk4oui-shard-00-01.dfnd8so.mongodb.net:27017,ac-tgk4oui-shard-00-02.dfnd8so.mongodb.net:27017/?ssl=true&replicaSet=atlas-57qqm7-shard-0&authSource=admin&appName=PadelWebDatabase';
 
 mongoose.connect(dbURI)
     .then(async () => {
