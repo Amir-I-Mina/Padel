9d49a5ed2ffdd447b9b930fd59e2cf7da06af6b4
Amir-I-Mina <amir2401052@miuegypt.edu.eg>
2026-06-09 00:52:40 +0300

    comment line 11

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(fileUpload());
+//app.use(fileUpload());
 const session = require("express-session");
 
 app.use(session({
