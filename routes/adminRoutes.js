7de06da74019286a3425a912273d1846f1baadc0
youssefhonn <hanyyoussef753@gmail.com>
2026-06-09 04:21:38 +0300

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -12,11 +12,6 @@ router.post("/users/delete/:id", isAdmin, adminController.admin_deleteUser);
 router.post("/users/make-admin/:id", isAdmin, adminController.admin_makeAdmin);
 router.post("/users/remove-admin/:id", isAdmin, adminController.admin_removeAdmin);
 router.get("/academy", isAdmin, adminController.admin_getAcademyMenu);
-<<<<<<< Updated upstream
-router.post("/home-management",isAdmin,adminController.admin_update_homeManagement);
-
-router.get("/products", isAdmin, adminController.admin_get_products);
-=======
 
 router.get("/products", isAdmin, storeController.manage_get_products);
 router.get("/products/all", isAdmin, storeController.getAdminAllProducts);
@@ -24,6 +19,5 @@ router.post("/products/add", isAdmin, storeController.addProduct);
 router.post("/products/update/:id", isAdmin, storeController.updateProduct);
 router.post("/products/toggle-stock/:id", isAdmin, storeController.toggleStock);
 router.post("/products/delete/:id", isAdmin, storeController.deleteProduct);
->>>>>>> Stashed changes
 
 module.exports = router;
